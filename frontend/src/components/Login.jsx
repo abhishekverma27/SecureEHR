89b8ba759f87dd35a588a87ff06c77a07fc4a6ec
ABHISHEK VERMA <96565154+abhishekverma276@users.noreply.github.com>
2024-05-24 13:08:18 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     setShowAnimation(true);
 
     axios
-      .post('https://secuehrbackend-hy1spu6d6-abhishekverma276s-projects.vercel.app/login', { email, password, role }) // Updated URL
+      .post('https://secuehrbackend.vercel.app/login', { email, password, role }) // Updated URL
       .then((result) => {
         console.log(result);
         if (result.data === 'Success') {
