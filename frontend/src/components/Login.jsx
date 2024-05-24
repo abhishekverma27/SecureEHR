4f941da9b12be1cc2d9abc23e820ed0cc07d62f4
ABHISHEK VERMA <96565154+abhishekverma276@users.noreply.github.com>
2024-05-24 12:55:00 +0530

    Updated axios API endpoint to deployment URL Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     setShowAnimation(true);
 
     axios
-      .post('http://localhost:3001/login', { email, password, role })
+      .post('https://secuehrbackend-hy1spu6d6-abhishekverma276s-projects.vercel.app/login', { email, password, role }) // Updated URL
       .then((result) => {
         console.log(result);
         if (result.data === 'Success') {
